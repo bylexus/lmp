b52b0fd4ca36e747e64284ed6677bf064e9cd0c1
Alexander Schenkel <aschenkel@kadenpartner.ch>
2020-01-01 17:26:19 +0100

    Start over - Switching to C++/Qt

--- src/routes/router.js
+++
@@ -1,9 +0,0 @@
-import VueRouter from 'vue-router';
-import Home from 'views/home/View.vue';
-import Settings from 'views/settings/View.vue';
-
-const router = new VueRouter({
-    routes: [{ path: '/', component: Home }, { path: '/settings', component: Settings }],
-});
-
-export default router;
